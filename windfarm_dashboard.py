2dfa0e1a25f1ef0fc8ddf9ae86bb25b719dff628
Noel Peatfield <noel@unitymk.com>
2025-08-19 09:54:24 +0100

    Update windfarm_dashboard.py

--- windfarm_dashboard.py
+++ windfarm_dashboard.py
@@ -24,7 +24,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.title("Scottish Wind Farm Curtailment Dashboard - Prototype V1 (Corrected)")
+st.title("Scottish Wind Farm Curtailment Dashboard - Prototype V2")
 
 # --- Farm selector ---
 windfarms = df["Generator_Full_Name"].unique()
